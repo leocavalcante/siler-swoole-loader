d00781ed85217559a8503a4282e83d40da04d27a
leocavalcante <lc@leocavalcante.com>
2018-11-28 15:07:19 -0200

    Handle Loader.io Token

--- index.php
+++ index.php
@@ -17,10 +17,19 @@ function handle_data($res, $data) {
     };
 }
 
+function handle_token($res, string $token) {
+    return function () use ($res, $token) {
+        $res->end($token);
+    };
+}
+
 function handle() {
+    $config = parse_ini_file('.env');
+    $loaderIoToken = $config['LOADER_IO'];
     $data = fetch_data();
 
-    return function ($req, $res) use ($data) {
+    return function ($req, $res) use ($loaderIoToken, $data) {
+        Route\get("/$loaderIoToken", handle_token($res, $loaderIoToken), Swoole\cast($req));
         Route\get('/data', handle_data($res, $data), Swoole\cast($req));
     };
 }
